3781558ab5dfbd0a5318f7b1c6607f36681d0ecd
Rene Jonker <r.jonker@sxblue.nl>
2026-07-30 09:50:31 +0200

    feat: Aanpassing voor de demo sprint222

--- force-app/main/default/lwc/testWithError/testWithError.js
+++ force-app/main/default/lwc/testWithError/testWithError.js
@@ -1,4 +1,4 @@
 import { LightningElement } from 'lwc';
 
 export default class TestWithError extends LightningElement {}
-/* a change (agsain 30-07-2026) */
+/* a change (agsain 30-07-2026------) */
